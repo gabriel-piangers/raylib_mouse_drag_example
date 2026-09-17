21e9c8c056d3191fec62cfb95cf088e8de5a99fb
gabriel-piangers <piangersgabriel@gmail.com>
2026-09-16 23:09:27 -0300

    Added Circle and Triangle implementation

--- src/main.c
+++ src/main.c
@@ -1,6 +1,6 @@
 /*******************************************************************************************
 *
-*   raylib [core] example - mouse drag and scaling
+*   raylib [core] example - mouse drag and drop
 *
 *   Example complexity rating: [★☆☆☆] 1/4
 *
@@ -18,6 +18,20 @@
 #include "raylib.h"
 #include "raymath.h"
 
+//----------------------------------------------------------------------------------
+// Types and Structures Definition
+//----------------------------------------------------------------------------------
+typedef struct {
+    Vector2 center;
+    float radius;
+} Circle;
+
+typedef struct {
+    Vector2 v1;
+    Vector2 v2;
+    Vector2 v3;
+} Triangle;
+
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
@@ -28,18 +42,24 @@ int main(void)
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    InitWindow(screenWidth, screenHeight, "raylib [core] example -  mouse drag ");
+    InitWindow(screenWidth, screenHeight, "raylib [core] example -  mouse drag and drop");
 
-    Rectangle boxRect = { screenWidth/2 - 40, screenHeight/2 - 40, 80.0f, 80.0f };
-    Rectangle boxScaled = boxRect;           // BoxRect with scale applied
-    Rectangle innerArea = { 300.0f, 300.0f, 70.0f, 70.0f };   
-    Rectangle outerArea = { 290.0f, 290.0f, 90.0f, 90.0f };   
-    bool pickedUp = false;
-    Vector2 mouseOffset = { 0.0f, 0.0f };    // Stores the offset of the mouse relative to the box's position
-    float boxScale = 1.0f;
-    float maxScale = 2.0f;
-    float minScale = 0.5f;
-    float scallingSpeed = 0.1f;
+    // Rectangle 
+    Rectangle rec = { screenWidth/2 - 250, screenHeight/2 + 50, 100.0f, 100.0f };
+    Rectangle recArea = { screenWidth/2 - 60, screenHeight/2 - 110, 110.0f, 110.0f };   
+    bool recPickedUp = false;
+
+    // Circle
+    Circle circ = { screenWidth/2, screenHeight/2 + 100, 50.0f };
+    Circle circArea = { screenWidth/2 - 195, screenHeight/2 - 55, 55.0f };
+    bool circPickedUp = false;
+
+    // Triangle
+    Triangle tri = { { 600, 282 }, { 550, 369 }, { 650, 369 } };
+    Triangle triArea = { { 600, 115 }, { 540, 222 }, { 660, 222 } };
+    bool triPickedUp = false;
+
+    Vector2 mouseOffset = { 0.0f, 0.0f };    // Stores the offset of the mouse relative to the object's pivot point
 
     SetTargetFPS(60);
     //--------------------------------------------------------------------------------------
@@ -50,39 +70,74 @@ int main(void)
         // Update
         //----------------------------------------------------------------------------------
         Vector2 mousePosition = GetMousePosition();
-        float wheelMove = GetMouseWheelMove();
-        bool placed = false;
+        bool recPlaced = false;
+        bool circPlaced = false;
+        bool triPlaced = false;
 
-        // Input
-        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(mousePosition, boxScaled)) {
-            pickedUp = true;
-            mouseOffset = (Vector2) { boxRect.x - mousePosition.x, boxRect.y - mousePosition.y };
-        }
-        if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT) && pickedUp) {
-            pickedUp = false;
+        if(IsMouseButtonPressed(MOUSE_BUTTON_RIGHT)) {
+            printf("%.2f, %.2f\n", mousePosition.x, mousePosition.y);
         }
 
-        // Box update
-        if (pickedUp) {
-            boxRect.x = mousePosition.x + mouseOffset.x;
-            boxRect.y = mousePosition.y + mouseOffset.y;
-        }
-        if (wheelMove && CheckCollisionPointRec(mousePosition, boxScaled)) {
-            boxScale = Clamp(boxScale + wheelMove*scallingSpeed, minScale, maxScale);
-        }
-
-        Rectangle innerCol = GetCollisionRec(innerArea, boxScaled);
-        Rectangle outerCol = GetCollisionRec(outerArea, boxScaled);
-        if (innerCol.width == innerArea.width && innerCol.height == innerArea.height) {
-            if (outerCol.width == boxRect.width && outerCol.height == outerCol.height) {
-               placed = true;
-            }
+        // Rectangle input
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointRec(mousePosition, rec)) {
+            recPickedUp = true;
+            mouseOffset = (Vector2) { rec.x - mousePosition.x, rec.y - mousePosition.y };
+        } else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT) && recPickedUp) {
+            recPickedUp = false;
         }
 
-        boxScaled.width = boxRect.width * boxScale;
-        boxScaled.height = boxRect.height * boxScale;
-        boxScaled.x = boxRect.x - boxRect.width*(boxScale - 1)/2;
-        boxScaled.y = boxRect.y - boxRect.height*(boxScale - 1)/2;
+        // Circle input
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointCircle(mousePosition, circ.center, circ.radius)) {
+            circPickedUp = true;
+            mouseOffset = (Vector2) { circ.center.x - mousePosition.x, circ.center.y - mousePosition.y };
+        } else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT) && circPickedUp) {
+            circPickedUp = false;
+        }
+
+        // Triangle input
+        if (IsMouseButtonPressed(MOUSE_BUTTON_LEFT) && CheckCollisionPointTriangle(mousePosition, tri.v1, tri.v2, tri.v3)) {
+            triPickedUp = true;
+            mouseOffset = (Vector2) { tri.v1.x - mousePosition.x, tri.v1.y - mousePosition.y }; // Uses v1 as the pivot point
+        } else if (IsMouseButtonReleased(MOUSE_BUTTON_LEFT) && triPickedUp) {
+            triPickedUp = false;
+        }
+
+        // Rectangle update
+        if (recPickedUp) {
+            rec.x = mousePosition.x + mouseOffset.x;
+            rec.y = mousePosition.y + mouseOffset.y;
+        }
+        Rectangle RecCol = GetCollisionRec(recArea, rec);
+        if (RecCol.width == rec.width && RecCol.height == rec.height) {
+            recPlaced = true;
+        }        
+
+        // Circle update
+        if (circPickedUp) {
+            circ.center.x = mousePosition.x + mouseOffset.x;
+            circ.center.y = mousePosition.y + mouseOffset.y;
+        }
+        if (Vector2Distance(circ.center, circArea.center) < circArea.radius - circ.radius) {
+            circPlaced = true;
+        }
+
+        // Triangle update
+        if (triPickedUp) {
+            Vector2 v2Offset = { tri.v2.x - tri.v1.x, tri.v2.y - tri.v1.y };
+            Vector2 v3Offset = { tri.v3.x - tri.v1.x, tri.v3.y - tri.v1.y };
+            
+            tri.v1 = (Vector2) { mousePosition.x + mouseOffset.x, mousePosition.y + mouseOffset.y };
+            tri.v2 = (Vector2) { tri.v1.x + v2Offset.x, tri.v1.y + v2Offset.y };
+            tri.v3 = (Vector2) { tri.v1.x + v3Offset.x, tri.v1.y + v3Offset.y };
+        }
+        if (
+            CheckCollisionPointTriangle(tri.v1, triArea.v1, triArea.v2, triArea.v3) &&
+            CheckCollisionPointTriangle(tri.v2, triArea.v1, triArea.v2, triArea.v3) &&
+            CheckCollisionPointTriangle(tri.v2, triArea.v1, triArea.v2, triArea.v3)
+        ) {
+            triPlaced = true;
+        }
+        //----------------------------------------------------------------------------------
 
         // Draw
         //----------------------------------------------------------------------------------
@@ -90,13 +145,15 @@ int main(void)
 
             ClearBackground(RAYWHITE);
 
-            DrawRectangleLinesEx(outerArea, 2.0f, placed ? GREEN : RED);
-            DrawRectangleRec(innerArea, YELLOW);
+            DrawRectangleLinesEx(recArea, 2.0f, recPlaced ? GREEN : RED);
+            DrawCircleLinesEx(circArea.center, circArea.radius, 2.0f, circPlaced ? GREEN : RED);
+            DrawTriangleLinesEx(triArea.v1, triArea.v2, triArea.v3, 2.0f, triPlaced ? GREEN : RED);
 
-            DrawRectangleRec(boxScaled, BROWN);
+            DrawRectangleRec(rec, ORANGE);
+            DrawCircleV(circ.center, circ.radius, BLUE);
+            DrawTriangle(tri.v1, tri.v2, tri.v3, VIOLET);
 
-            DrawText("Use mouse to drag and drop the box and mouse wheel to scale!", 10, 10, 20, GRAY);
-            DrawText(TextFormat("Box scale: %.2f", boxScale), 10, 40, 20, LIGHTGRAY);
+            DrawText("Use mouse to drag and drop the objects into the right spot!", 10, 10, 20, GRAY);
         EndDrawing();
         //----------------------------------------------------------------------------------
     }
